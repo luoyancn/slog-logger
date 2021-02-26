8ef08b55dc3967cda21070c7d4a941099235e81a
luoyancn <luoyan_cn@outlook.com>
2021-02-26 14:50:04 +0800

    Opt the codes

--- src/lib.rs
+++ src/lib.rs
@@ -13,37 +13,63 @@ use slog_filerotate::FileAppender;
 
 const TIMESTAMP_FORMAT: &str = "%Y-%m-%d %H:%M:%S%.9f";
 
+pub const BITE: u64 = 1;
+pub const KB: u64 = BITE * 1024;
+pub const MB: u64 = KB * 1024;
+pub const GB: u64 = MB * 1024;
+
 fn timestamp_custom(io: &mut dyn io::Write) -> io::Result<()> {
     write!(io, "{}", chrono::Local::now().format(TIMESTAMP_FORMAT))
 }
 
-pub fn initlogger(duplicate: bool, logfile: &str, filesize: u64) -> slog::Logger {
+pub fn initlogger(
+    duplicate: bool,
+    logfile: &str,
+    filesize: u64,
+    debug: bool,
+    detail: bool,
+) -> slog::Logger {
     let decorator = slog_term::TermDecorator::new().build();
     let drain = Mutex::new(
         slog_term::FullFormat::new(decorator)
             .use_custom_timestamp(timestamp_custom)
             .build(),
     );
+    let mut drain_filter = slog::LevelFilter::new(drain, slog::Level::Trace);
+    if !debug {
+        drain_filter = drain_filter.filter_level(slog::Level::Info).0;
+    }
     if duplicate {
         let adapter = FileAppender::new(logfile, false, filesize, 2, true);
-
         let decorator_file = slog_term::PlainSyncDecorator::new(adapter);
         let drain_file = slog_term::FullFormat::new(decorator_file)
             .use_custom_timestamp(timestamp_custom)
             .build();
-        slog::Logger::root(
-            slog::Duplicate::new(
-                slog::LevelFilter::new(drain_file, slog::Level::Trace),
-                slog::LevelFilter::new(drain, slog::Level::Trace),
+        let mut drain_file_filter = slog::LevelFilter::new(drain_file, slog::Level::Trace);
+        if !debug {
+            drain_file_filter = drain_file_filter.filter_level(slog::Level::Info).0;
+        }
+
+        if !detail {
+            slog::Logger::root(
+                slog::Duplicate::new(drain_file_filter, drain_filter).fuse(),
+                o!(),
             )
-            .fuse(),
-            o!("place" => FnValue( move |info| { format!("{}:{} {}", info.file(), info.line(), info.module())})),
-        )
+        } else {
+            slog::Logger::root(
+                slog::Duplicate::new(drain_file_filter, drain_filter).fuse(),
+                o!("place" => FnValue( move |info| { format!("{}:{} {}", info.file(), info.line(), info.module())})),
+            )
+        }
     } else {
-        slog::Logger::root(
-            slog::LevelFilter::new(drain, slog::Level::Trace).fuse(),
-            o!("place" => FnValue( move |info| { format!("{}:{} {}", info.file(), info.line(), info.module())})),
-        )
+        if !detail {
+            slog::Logger::root(drain_filter.fuse(), o!())
+        } else {
+            slog::Logger::root(
+                drain_filter.fuse(),
+                o!("place" => FnValue( move |info| { format!("{}:{} {}", info.file(), info.line(), info.module())})),
+            )
+        }
     }
 }
 
